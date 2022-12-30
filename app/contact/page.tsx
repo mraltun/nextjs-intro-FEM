eeec49a313a3ace1b65e5cb3ff908d56e1f8a257
Murat Altun <mraltun@gmail.com>
2022-12-30 15:07:54 +0300

    Added Navigations to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,24 @@
+// Change it from Server Component to Client Component
+"use client";
+// Normal navigation
+import Link from "next/link";
+// Programatic navigation (only works in client components because window, hence use client above)
+import { useRouter } from "next/navigation";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const post = { title: "Test" };
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>Contact</h1>
+      <Link href='/'>home</Link>
+      <Link href={`/blog/${post.title}`}>post</Link>
+      <button type='button' onClick={() => router.push("/blog")}>
+        Blog
+      </button>
+    </div>
+  );
 };
 
 export default Contact;
